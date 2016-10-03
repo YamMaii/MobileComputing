a17230dc8bc775eea967996192fa2f5b9834cd46
Priam Thaddeus B. Alcala <YamMaii@users.noreply.github.com>
2016-10-04 00:39:07 +0800

    Show Password

--- app/src/main/java/com/example/priam/mobilecomputingactivities/MainActivity.java
+++ app/src/main/java/com/example/priam/mobilecomputingactivities/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.priam.mobilecomputingactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.view.MotionEvent;
 
 import java.util.regex.Pattern;
 
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         final EditText emailValidate = (EditText) findViewById(R.id.emailAdress_text);
         final EditText passwordFormat = (EditText) findViewById(R.id.password_text);
         final Button button = (Button) findViewById(R.id.loginBtn);
+        final TextView showBtn = (TextView) findViewById(R.id.showBtn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,6 +49,24 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        showBtn.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event){
+                                switch ( event.getAction() ) {
+
+                                                case MotionEvent.ACTION_DOWN:
+                                                EditText pText=(EditText)findViewById(R.id.password_text);
+                                                pText.setInputType(InputType.TYPE_CLASS_TEXT);
+                                                break;
+                                        case MotionEvent.ACTION_UP:
+                                                EditText aText=(EditText)findViewById(R.id.password_text);
+                                                aText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                                                break;
+                                    }
+                                return true;
+                            }
+
+                            });
     }
 
         private boolean validate(String email){
@@ -56,5 +78,5 @@ public class MainActivity extends AppCompatActivity {
                                    + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
                                    + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$").matcher(email).matches();
         }
-    }
 }
+
